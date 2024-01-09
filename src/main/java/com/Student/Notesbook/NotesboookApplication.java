bf3506be54352784dc84006e1b7550345c00197a
Kshitij umarkar <kshitijumarkar@gmail.com>
2024-01-09 16:13:00 +0530

    NotesBook project setup

---
+++ src/main/java/com/Student/Notesbook/NotesboookApplication.java
@@ -0,0 +1,13 @@
+package com.Student.Notesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesboookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesboookApplication.class, args);
+	}
+
+}
