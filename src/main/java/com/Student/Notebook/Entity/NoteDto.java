c0c11a76db54516809739f0661a2e3af4a69e9b9
Kshitij umarkar <kshitijumarkar@gmail.com>
2024-01-15 15:04:05 +0530

    Notesbook application controller updated

--- src/main/java/com/Student/Notebook/Entity/NoteDto.java
+++ src/main/java/com/Student/Notebook/Entity/NoteDto.java
@@ -2,4 +2,36 @@ package com.Student.Notebook.Entity;
 
 public class NoteDto {
 
+    private String title;
+    private String content;
+
+    // Constructors, getters, and setters
+
+    public NoteDto() {
+        // Default constructor
+    }
+
+    public NoteDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
+
--- src/main/java/com/Student/Notebook/Notebookservice/Notebookservice.java
+++ src/main/java/com/Student/Notebook/Notebookservice/Notebookservice.java
@@ -1,5 +1,57 @@
 package com.Student.Notebook.Notebookservice;
 
-public interface Notebookservice {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Student.Notebook.Entity.Note;
+import com.Student.Notebook.Entity.NoteDto;
+import com.Student.Notebook.Repository.NotesbookRepository;
+
+import java.util.List;
+
+@Service
+public class Notebookservice {
+
+    private final NotesbookRepository notesbookRepository;
+
+    @Autowired
+    public Notebookservice(NotesbookRepository notesbookRepository) {
+        this.notesbookRepository = notesbookRepository;
+    }
+
+    public List<Note> getAllNotes() {
+        return notesbookRepository.findAll();
+    }
+
+    public Note getNoteById(Long id) {
+        return notesbookRepository.findById(id).orElse(null);
+    }
+
+    public Note createNote(NoteDto noteDto) {
+        Note note = new Note();
+        note.setTitle(noteDto.getTitle());
+        note.setContent(noteDto.getContent());
+        return notesbookRepository.save(note);
+    }
+
+    public Note updateNote(Long id, NoteDto noteDto) {
+        Note existingNote = notesbookRepository.findById(id).orElse(null);
+        if (existingNote != null) {
+            existingNote.setTitle(noteDto.getTitle());
+            existingNote.setContent(noteDto.getContent());
+            return notesbookRepository.save(existingNote);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteNote(Long id) {
+        if (notesbookRepository.existsById(id)) {
+            notesbookRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
